db57e0674a799ab93981debb7a2d391dbdaa2857
yuji shimada <yuji@yuji-no-MacBook-Pro.local>
2017-05-06 00:07:12 +0900

    add apk directory.

--- travis/uploader.sh
+++ travis/uploader.sh
@@ -5,11 +5,11 @@ echo "--- uploader start ---";
 TOKEN=$1
 SRC_FILE=$2
 FILE_NAME=basename $SRC_FILE
-DST_FILE=$3$FILE_NAME
+DST_FILE=$3
 
 curl -f -I -X POST \
        -H "Authorization: Bearer $TOKEN" \
        -T  "$SRC_FILE" \
-       "https://content.dropboxapi.com/1/files_put/auto/$DST_FILE"
+       "https://content.dropboxapi.com/1/files_put/auto/$DST_FILE/$FILE_NAME"
 
 echo "\n--- uploader end ---";
