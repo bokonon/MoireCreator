dd87d8e8fb73057e3548822f92a64649816d4b14
yuji shimada <yuji@yuji-no-MacBook-Pro.local>
2017-05-06 22:59:55 +0900

    modify uploader.sh.

--- travis/uploader.sh
+++ travis/uploader.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
 
-echo "--- uploader start ---";
+echo '--- uploader start ---';
 
 TOKEN=$1
 SRC_FILE=$2
 FILE_NAME=basename $SRC_FILE
 DST_FILE=$3
-DATE=date '+%Y/%m/%d/%R'
+DATE=date "+%Y/%m/%d-%R"
 
 curl -f -I -X POST \
        -H "Authorization: Bearer $TOKEN" \
        -T  "$SRC_FILE" \
        "https://content.dropboxapi.com/1/files_put/auto/$DST_FILE/$DATE/$FILE_NAME"
 
-if [ `echo $FILE_NAME|grep release` ]; then
-       echo "--- uploader mapping.text start ---";
+if [ "echo $FILE_NAME|grep debug" ]; then
+       echo '--- uploader mapping.text start ---';
 
        curl -f -I -X POST \
               -H "Authorization: Bearer $TOKEN" \
               -T  ./app/build/outputs/mapping/release/mapping.txt \
               "https://content.dropboxapi.com/1/files_put/auto/$DST_FILE/$DATE/mapping.txt"
 
-       echo "--- uploader mapping.text end ---";
+       echo '--- uploader mapping.text end ---';
 fi
 
-echo "--- uploader end ---";
+echo '--- uploader end ---';
