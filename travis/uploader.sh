fb219488f85bfbaae1eeb34e5abdecd37f5ab3ad
yuji shimada <yuji@yuji-no-MacBook-Pro.local>
2017-05-07 13:27:09 +0900

    add time zone.

--- travis/uploader.sh
+++ travis/uploader.sh
@@ -6,7 +6,7 @@ TOKEN=$1
 SRC_FILE=$2
 FILE_NAME=basename $SRC_FILE
 DST_FILE=$3
-DATE=`Asia/Tokyo date +%Y%m%d-%H%M%S`
+DATE=Asia/Tokyo date +"%Y%m%d-%H%M%S"
 
 curl -f -I -X POST \
        -H "Authorization: Bearer $TOKEN" \
