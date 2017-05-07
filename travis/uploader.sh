ec2f183d6e1964109456975b335319acae95b64f
yuji shimada <yuji@yuji-no-MacBook-Pro.local>
2017-05-07 13:56:24 +0900

    modify time zone.

--- travis/uploader.sh
+++ travis/uploader.sh
@@ -6,12 +6,12 @@ TOKEN=$1
 SRC_FILE=$2
 FILE_NAME=basename $SRC_FILE
 DST_FILE=$3
-DATE=Asia/Tokyo date +"%Y%m%d-%H%M%S"
+DATE=Asia/Tokyo date +"%Y%m%d-%H:%M"
 
 curl -f -I -X POST \
        -H "Authorization: Bearer $TOKEN" \
        -T  "$SRC_FILE" \
-       "https://content.dropboxapi.com/1/files_put/auto/$DST_FILE/${DATE}/$FILE_NAME"
+       "https://content.dropboxapi.com/1/files_put/auto/$DST_FILE/$DATE/$FILE_NAME"
 
 if [ "echo $FILE_NAME|grep debug" ]; then
        echo '--- uploader mapping.text start ---';
@@ -19,7 +19,7 @@ if [ "echo $FILE_NAME|grep debug" ]; then
        curl -f -I -X POST \
               -H "Authorization: Bearer $TOKEN" \
               -T "./app/build/outputs/mapping/release/mapping.txt" \
-              "https://content.dropboxapi.com/1/files_put/auto/$DST_FILE/${DATE}/mapping.txt"
+              "https://content.dropboxapi.com/1/files_put/auto/$DST_FILE/$DATE/mapping.txt"
 
        echo '--- uploader mapping.text end ---';
 fi
