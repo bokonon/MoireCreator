47960c7debd0b111b92e1af36e3cb444e7a20e11
yuji shimada <yuji@yuji-no-MacBook-Pro.local>
2017-05-13 18:35:33 +0900

    modify gradle.properties.

--- app/src/main/java/ys/moire/presentation/ui/other/OtherActivity.java
+++ app/src/main/java/ys/moire/presentation/ui/other/OtherActivity.java
@@ -2,7 +2,9 @@ package ys.moire.presentation.ui.other;
 
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
 
+import com.google.android.gms.ads.AdListener;
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
 
@@ -24,6 +26,24 @@ public class OtherActivity extends BaseActivity {
         setSupportActionBar(toolbar);
 
         final AdView adView = (AdView)findViewById(R.id.ad_view);
+        adView.setAdListener(new AdListener() {
+            @Override
+            public void onAdClosed() {
+            }
+            @Override
+            public void onAdFailedToLoad(int errorCode) {
+            }
+            @Override
+            public void onAdLeftApplication() {
+            }
+            @Override
+            public void onAdOpened() {
+            }
+            @Override
+            public void onAdLoaded() {
+                adView.setVisibility(View.VISIBLE);
+            }
+        });
         AdRequest adRequest = new AdRequest.Builder().build();
         adView.loadAd(adRequest);
     }
